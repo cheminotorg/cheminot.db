c1bc091ef03a2c2bd9a58324984982f589a9130e
Sebastien Renault <sre@zenexity.com>
2015-02-16 22:37:32 +0100

    Had a very strange error. Launching app from jar was very slow. This was due to the 'scala-parser-combinators' pulled from 'play-json' depencencies.

--- project/Build.scala
+++ project/Build.scala
@@ -10,13 +10,13 @@ object CheminotDB extends Build {
       scalaVersion := "2.11.4",
       resolvers += "Typesafe repository releases" at "http://repo.typesafe.com/typesafe/releases/",
       libraryDependencies ++= Seq(
-        "com.typesafe.play" %% "play-json" % "2.4.0-M1",
-        "com.typesafe.play" %% "anorm" % "2.4.0-M1",
-        "commons-codec" % "commons-codec" % "1.9",
+        "com.typesafe.play" %% "play-json" % "2.4.0-M2",
+        "com.typesafe.play" %% "anorm" % "2.4.0-M2",
         "commons-io" % "commons-io" % "2.4",
         "org.xerial" % "sqlite-jdbc" % "3.8.6",
         "com.github.scopt" %% "scopt" % "3.2.0",
-        "org.scalaj" %% "scalaj-http" % "1.1.4"
+        "org.scalaj" %% "scalaj-http" % "1.1.4",
+        "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.3"
       ),
       scalacOptions ++= Seq("-language:_", "-deprecation", "-unchecked"))
   ).settings(com.github.retronym.SbtOneJar.oneJarSettings: _*)
