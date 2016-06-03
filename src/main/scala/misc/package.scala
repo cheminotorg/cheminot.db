591a3ee6fe3895ddb30af646bd22cc2e54c43d54
Sebastien Renault <srenault.contact@gmail.com>
2016-06-03 18:35:53 +0200

    Enhance progress bar

--- src/main/scala/misc/package.scala
+++ src/main/scala/misc/package.scala
@@ -11,22 +11,31 @@ package m.cheminot {
 
     implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(THREADS_PER_POOL))
 
+    def displayProgress(progress: Int) = {
+      val p = (progress * 70) / 100
+      val todo = 70 - p
+      val total = List.fill(p)("#") ++: List.fill(todo)(" ")
+      print(s"[${total.mkString}] ${p}%\r")
+    }
+
     def par[A, B](aaa: Seq[A], debug: Boolean = false)(f: (A) => B)(implicit ec: ExecutionContext): Seq[B] = {
       val n = 20 //aaa.size / (THREADS_PER_POOL * 2)
       if(debug) { println(s"[progress] total: ${aaa.size} | grouped: ${n}") }
       val counter = new AtomicInteger(0);
-      Await.result(
+      val result = Await.result(
         misc.FutureUtils.groupSequentially(aaa, n) { a =>
           Future(f(a)).andThen {
             case _ =>
               if(debug) {
                 val progress = scala.math.round(counter.incrementAndGet.toFloat / aaa.size.toFloat * 100)
-                println(s"[progress] ${progress}%")
+                displayProgress(progress.toInt)
               }
           }
         },
         1.hours
       )
+      println("");
+      result
     }
   }
 }
